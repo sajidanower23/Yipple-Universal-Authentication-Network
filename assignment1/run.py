1a49483b76698e556eb2327f1f5b24e0a04cb874
Sajid Ibne Anower <sajidanower23@gmail.com>
2018-03-16 15:18:29 +1100

    Add option to run app in dev

--- assignment1/run.py
+++ assignment1/run.py
@@ -3,5 +3,11 @@
 from flaskr import create_app
 app = create_app()
 
+# This should be a cmd arg
+is_dev = True
+
 if __name__ == "__main__":
-    app.run(port=9447, debug=True, host='0.0.0.0')
+    if is_dev:
+        app.run(port=9447, debug=True)        
+    else:
+        app.run(port=9447, debug=False, host='0.0.0.0')
